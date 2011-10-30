ac19ef2720842531194c0c3c0ed8d425893f9b85
fletchto99 <fletchto99@hotmail.com>
2011-10-30 14:55:24 -0700

    update to settings

--- src/org/monstercraft/irc/IRC.java
+++ src/org/monstercraft/irc/IRC.java
@@ -1,9 +1,11 @@
 package org.monstercraft.irc;
 
 import java.io.File;
+
 import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.monstercraft.irc.command.Commands;
 import org.monstercraft.irc.handlers.IRCHandler;
@@ -14,12 +16,14 @@ public class IRC extends JavaPlugin {
 
 	public static Server server = null;
 	public Settings settings = null;
+	public static FileConfiguration config;
 
 	public void onEnable() {
 		server = getServer();
+		config = getConfig();
 		settings = new Settings();
-		File config = new File(Constants.SETTINGS_PATH + Constants.SETTINGS_FILE);
-		if (!config.exists()) {
+		File con = new File(Constants.SETTINGS_PATH + Constants.SETTINGS_FILE);
+		if (!con.exists()) {
 			new File(Constants.SETTINGS_PATH).mkdirs();
 			settings.GenConfig();
 		} else {
--- src/org/monstercraft/irc/util/Settings.java
+++ src/org/monstercraft/irc/util/Settings.java
@@ -1,18 +1,22 @@
 package org.monstercraft.irc.util;
 
+import java.io.File;
+
 import org.monstercraft.irc.IRC;
 
-public class Settings extends IRC {
+public class Settings {
 
 	public void GenConfig() {
 		try {
-			getConfig().set("PORT", 6667);
-			getConfig().set("SERVER", "irc.esper.net");
-			getConfig().set("CHANNEL", "#Monstercraft");
-			getConfig().set("NAME", "MonsterCraft");
-			getConfig().set("LOGIN", "MC");
-			getConfig().set("MESSAGE_LIMIT", 5);
-			getConfig().save(Constants.SETTINGS_PATH + Constants.SETTINGS_FILE);
+			IRC.config.options().copyDefaults();
+			IRC.config.set("irc.port", 6667);
+			IRC.config.set("irc.server", "irc.esper.net");
+			IRC.config.set("irc.channel", "#Monstercraft");
+			IRC.config.set("irc.name", "MonsterCraft");
+			IRC.config.set("irc.login", "MC");
+			IRC.config.set("irc.messagelimit", 5);
+			IRC.config.save(new File(Constants.SETTINGS_PATH
+					+ Constants.SETTINGS_FILE));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -20,14 +24,16 @@ public class Settings extends IRC {
 
 	public void LoadConfigs() {
 		try {
-			getConfig().load(Constants.SETTINGS_PATH + Constants.SETTINGS_FILE);
-			Variables.port = getConfig().getInt("PORT", 6667);
-			Variables.server = getConfig().getString("SERVER", "irc.esper.ner");
-			Variables.channel = getConfig().getString("CHANNEL",
+			IRC.config.load(new File(Constants.SETTINGS_PATH
+					+ Constants.SETTINGS_FILE));
+			Variables.port = IRC.config.getInt("irc.port", 6667);
+			Variables.server = IRC.config.getString("irc.server",
+					"irc.esper.ner");
+			Variables.channel = IRC.config.getString("irc.channel",
 					"#Monstercraft");
-			Variables.name = getConfig().getString("NAME", "MonsterCraft");
-			Variables.login = getConfig().getString("LOGIN", "MC");
-			Variables.amount = getConfig().getInt("MESSAGE_LIMIT", 5);
+			Variables.name = IRC.config.getString("irc.name", "MonsterCraft");
+			Variables.login = IRC.config.getString("irc.login", "MC");
+			Variables.amount = IRC.config.getInt("irc.messagelimit", 5);
 		} catch (Exception e) {
 			GenConfig();
 		}
